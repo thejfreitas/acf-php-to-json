22a97ff9be57f82f289e980cc8830a081f11dced
J. de Freitas <juniormfreitas@gmail.com>
2020-08-30 10:23:59 -0600

    Define new constants

--- acf-php-to-json.php
+++ acf-php-to-json.php
@@ -15,6 +15,38 @@
 
 if( ! defined( 'ABSPATH' ) ) exit;
 
+if (! defined('ACF_PHP_TO_JSON_VERSION') ) {
+    define('ACF_PHP_TO_JSON_VERSION', '0.1.1');
+}
+
+if (! defined('ACF_PHP_TO_JSON_NAME') ) {
+    define('ACF_PHP_TO_JSON_NAME', 'ACF PHP to JSON Converter');
+}
+
+if (! defined('ACF_PHP_TO_JSON_PAGE_TITLE') ) {
+    define('ACF_PHP_TO_JSON_PAGE_TITLE', 'ACF - Convert PHP migration fields to JSON');
+}
+
+if (! defined('ACF_PHP_TO_JSON_SLUG') ) {
+    define('ACF_PHP_TO_JSON_SLUG', 'acf-php-to-json');
+}
+
+if (! defined('ACF_PHP_TO_JSON_POST_TYPE') ) {
+    define('ACF_PHP_TO_JSON_POST_TYPE', 'acf-field-group');
+}
+
+if (! defined('ACF_PHP_TO_JSON_MENU_TITLE') ) {
+    define('ACF_PHP_TO_JSON_MENU_TITLE', 'Convert PHP to JSON');
+}
+
+if (! defined('ACF_PHP_TO_JSON_BASENAME') ) {
+    define('ACF_PHP_TO_JSON_BASENAME', plugin_basename( __FILE__ ));
+}
+
+if (! defined('ACF_PHP_TO_JSON_PLUGIN_DIR') ) {
+    define('ACF_PHP_TO_JSON_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+}
+
 function acf_php_to_json_scripts() {
     wp_enqueue_media();
     wp_enqueue_style( 'acf-php-to-json', plugins_url('/admin/css/acf-php-to-json.css', __FILE__), array(), null);
